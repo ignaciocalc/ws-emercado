e044652e431d0700aa672fc76f22628cc1e04435
ignaciocalc <ignaciocal13@gmail.com>
2025-08-20 19:10:35 -0300

    correcciones en login.html y implementacion de verificacion con login.js

--- js/login.js
+++ js/login.js
@@ -2,24 +2,33 @@ const
    usuario = document.getElementById("user"),
    contraseña = document.getElementById("password"),
    boton = document.getElementById("ingresarLogin"),
-   parrafos = document.querySelectorAll(".aclaracionAlerta");
+   check = document.getElementById("checkRecordar"),
+   label = document.getElementById("labelCheck"),
+   parrafos = document.querySelectorAll(".claseError");
 
 boton.addEventListener('click', function(){
-   usuario.classList.replace("claseError", "claseInput");
-   contraseña.classList.replace("claseError", "claseInput");
+   usuario.classList.replace("camposError", "camposInput");
+   contraseña.classList.replace("camposError", "camposInput");
+   label.style.color = '#000000';
+
    for (let p of parrafos)
       p.style.display='none';
 
-   if (usuario.value != "" && contraseña.value != "") {
+   if (usuario.value != "" && contraseña.value != "" && check.checked) {
       window.location.href = "../index.html"
    } else {
       if (usuario.value == "") {
-         usuario.classList.replace("claseInput", "claseError");
+         usuario.classList.replace("camposInput", "camposError");
          parrafos[0].style.display="block";
       }
+      
       if (contraseña.value == "") {
-         contraseña.classList.replace("claseInput", "claseError");
+         contraseña.classList.replace("camposInput", "camposError");
          parrafos[1].style.display="block"
+      }
+
+      if (!check.checked) {
+         label.style.color = "#de514e";
       }
    }
 })
