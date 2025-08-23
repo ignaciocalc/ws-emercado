4145a234f7957df87936baa2d4fbbf0c5c052098
Tacuabé Ferrari <inti.ferrari@fing.edu.uy>
2025-08-22 21:07:23 -0300

    Implementacion de fetch y disenio de categoria

--- js/products.js
+++ js/products.js
@@ -1,67 +1,55 @@
 const PRODUCTS_CAT101 = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 let currentProductsArray = [];
-let desc;
-let name;
-let soldCount;
-let currency;
-let cost;
-let img;
 
 
-// funcion para testear
-function readData(){
+// recorre el listado actual de productos y crea la publicacion
+// con la funcion createItemCard()
+function writeData(){
     for(let i = 0; i < currentProductsArray.length; i++){
-        console.log(currentProductsArray[i].description)
-    }
- 
-    // console.log(currentProductsArray.products[0].name);
+        createItemCard(currentProductsArray[i].name, currentProductsArray[i].description, currentProductsArray[i].cost, currentProductsArray[i].currency, currentProductsArray[i].image, currentProductsArray[i].soldCount);
+    }    
 }
 
+// hace el fetch del link y llena el array
 document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_CAT101).then(function(resultObj){
         if(resultObj.status === "ok"){
             currentProductsArray = resultObj.data.products;
-            readData();
+            writeData();
+
         }
     });
-
-    desc = currentProductsArray[0].description;
-    name = currentProductsArray[0].name;
-    soldCount = currentProductsArray[0].soldCount;
-    currency = currentProductsArray[0].currency;
-    cost = currentProductsArray[0].cost;
-    img = currentProductsArray[0].image;
 
 })
 
 
-// crear sections para las cards en el article id="contenedorItem"
-// LISTAA
+function insertarHtml(contenedor, html){
+    document.getElementById(contenedor).appendChild(html);
 
+};
 
-// esta funcion crea e inserta el html
+// crea la publicacion dentro de las categorias
 function createItemCard(name, description, cost, currency, img, soldCount){
-    
-    // <section class="card">
-    
-    htmlToInsert = `    
-        <div class="card">
-            <img src="${img}" style="width:100%"/>
+        
+    let htmlToInsert = document.createElement("section");
+    htmlToInsert.classList.add('card');
+
+    htmlToInsert.innerHTML = `    
+            <img src="${img}" class="card-img"/>
+
+            <div class="card-cuerpo">
                 <h1>${name}</h1>
-                <p>${soldCount}</p>
-                <p>${description}</p>
-                <p>${currency}${cost}</p>
-                <p><button>Agregar al carrito </button></p>
-        </div>
-    `
-    // </section>
+                <p id="cantVendidos">${soldCount} vendidos</p>
+                <p id="descripcion">${description}</p>
+            </div>
 
-    // document.getElementById("contenedoritem").appendChild(htmlToInsert);
-    document.getElementsByTagName("main")[0].appendChild(htmlToInsert);
-
-    console.log("ENTRA A LA FUNCION");
+            <div class="card-footer">
+                <p class="card-precio">${currency} ${cost}</p>
+                <p class="card-boton"><button class="button">Agregar al carrito </button></p>
+            </div>                
+            
+    `;
+        
+    insertarHtml("contenedorItem", htmlToInsert);
 }
-
-
-createItemCard(name, desc, cost, currency, img, soldCount);
 
