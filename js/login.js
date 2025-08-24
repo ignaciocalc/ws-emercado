3546216f425fbe14cffeba57e0ae9ae9cf77be09
ignaciocalc <ignaciocal13@gmail.com>
2025-08-24 10:00:17 -0300

    optimizacion de validacion de login js

--- js/login.js
+++ js/login.js
@@ -5,12 +5,6 @@ const
    parrafos = document.querySelectorAll(".claseError");
 
 boton.addEventListener('click', function(){
-   usuario.classList.replace("camposError", "camposInput");
-   contraseña.classList.replace("camposError", "camposInput");
-
-   for (let p of parrafos)
-      p.style.display='none';
-
    if (usuario.value != "" && contraseña.value != "") {
       localStorage.setItem("user", usuario.value);
       window.location.href = "index.html";
@@ -18,11 +12,17 @@ boton.addEventListener('click', function(){
       if (usuario.value == "") {
          usuario.classList.replace("camposInput", "camposError");
          parrafos[0].style.display="block";
+      } else {
+         usuario.classList.replace("camposError", "camposInput");
+         parrafos[0].style.display="none";
       }
       
       if (contraseña.value == "") {
          contraseña.classList.replace("camposInput", "camposError");
          parrafos[1].style.display="block"
+      } else {
+         contraseña.classList.replace("camposError", "camposInput");
+         parrafos[1].style.display="none"
       }
    }
 })
