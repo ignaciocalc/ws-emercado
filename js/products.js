adeb55473af5736a405fc3e6ec18f53a6a3e626c
ignaciocalc <ignaciocal13@gmail.com>
2025-09-09 03:13:16 -0300

    diseños de filtros resolucion estandar implementado, falta version tablet y movil

--- js/products.js
+++ js/products.js
@@ -10,10 +10,10 @@ let USDollar = new Intl.NumberFormat('en-US', {
 
 // recorre el listado actual de productos y crea la publicacion
 // con la funcion createItemCard()
-function writeData(){
-    for(let i = 0; i < currentProductsArray.length; i++){
-        createItemCard(currentProductsArray[i].name, currentProductsArray[i].description, currentProductsArray[i].cost  /*USDollar.format(currentProductsArray[i].cost)*/, 
-            currentProductsArray[i].currency, currentProductsArray[i].image, currentProductsArray[i].soldCount, currentProductsArray[i].id) ;
+function writeData(arrayProd){
+    for(let i = 0; i < arrayProd.length; i++){
+        createItemCard(arrayProd[i].name, arrayProd[i].description, arrayProd[i].cost  /*USDollar.format(currentProductsArray[i].cost)*/, 
+            arrayProd[i].currency, arrayProd[i].image, arrayProd[i].soldCount, arrayProd[i].id) ;
     }    
 }
 
@@ -22,8 +22,7 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(PRODUCTS_CAT).then(function(resultObj){
         if(resultObj.status === "ok"){
             currentProductsArray = resultObj.data.products;
-            writeData();
-
+            writeData(resultObj.data.products);
         }
     });
 
@@ -67,5 +66,108 @@ function createItemCard(name, description, cost, currency, img, soldCount, ID){
     })
 }
 
+//Menu filtros
+const
+    mainProducts = document.getElementById("contPrincipal"),
+    contFiltros = document.getElementById("filtrosContenedor"),
+    flechaDesplegable = document.getElementById("flechaDesplegableImg"),
 
+    contOrdenarPor = document.getElementById("contOrdenarPor"),
+    flechaOrdenarPor = document.getElementById("flechaOrdenarPor"),
+    listaOrdPor = document.getElementById("lista-OrdenarPor"),
 
+    contFiltrarPrecio = document.getElementById("contBotonPrecio"),
+    flechaFiltrarPrecio = document.getElementById("flechaFiltrarPrecio"),
+    contMinMax = document.getElementById("contRangos"),
+    btnFiltrar = document.getElementById("BtnFiltrar"),
+
+    tipoFiltro = document.getElementById("tipoFiltro"),
+    imgTipoFiltro = document.getElementById("imgTipoFiltro"),
+
+    AZ = document.getElementById("AZ"),
+    ZA = document.getElementById("ZA"),
+    relevancia = document.getElementById("Relevancia"),
+    precioA = document.getElementById("PrecioA"),
+    precioD = document.getElementById("PrecioD");
+
+function setli() {
+    tipoFiltro.className = "mostrarfiltro";
+    imgTipoFiltro.className = "mostrarfiltro";
+
+    AZ.style.color = "black";
+    ZA.style.color = "black";
+    relevancia.style.color = "black";
+    precioA.style.color = "black";
+    precioD.style.color = "black";
+}
+
+flechaDesplegable.addEventListener('click', function(evento){
+    if (flechaDesplegable.className == "flechaDesplegable-oculto") {
+        flechaDesplegable.className = "flechaDesplegable-mostrar";
+        contFiltros.className = "filtrosContenedor-mostrar";
+        mainProducts.className = "mainProducts-FiltroMostrar";
+    } else {
+        flechaDesplegable.className = "flechaDesplegable-oculto";
+        contFiltros.className = "filtrosContenedor-oculto";
+        mainProducts.className = "mainProducts";
+    }
+    evento.stopPropagation();
+})
+
+contOrdenarPor.addEventListener('click', function(evento){
+    if (flechaOrdenarPor.className == "flechaOrdenarPor-mostrar") {
+        flechaOrdenarPor.classList = "flechaOrdenarPor-oculto";
+        listaOrdPor.classList = "lista-OrdenarPor-oculto"
+    } else {
+        flechaOrdenarPor.className = "flechaOrdenarPor-mostrar";
+        listaOrdPor.className = "lista-OrdenarPor-mostrar"
+    }
+    evento.stopPropagation();
+})
+
+contFiltrarPrecio.addEventListener('click', function(evento){
+    if (flechaFiltrarPrecio.className == "flechaPrecio-mostrar") {
+        flechaFiltrarPrecio.className = "flechaPrecio-oculto";
+        contMinMax.className = "rangos-ocultos";
+        btnFiltrar.className = "btn-oculto";
+    } else {
+        flechaFiltrarPrecio.className = "flechaPrecio-mostrar";
+        contMinMax.className = "rangos-mostrar";
+        btnFiltrar.className = "btn-mostrar";
+    }
+})
+
+AZ.addEventListener('click', function(){
+    tipoFiltro.textContent = "alfabeto";
+    setli();
+    AZ.style.color = "#3483fa";
+    imgTipoFiltro.src = "img/AZ.svg";
+})
+
+ZA.addEventListener('click', function(){
+    tipoFiltro.textContent = "alfabeto";
+    setli();
+    ZA.style.color = "#3483fa";
+    imgTipoFiltro.src = "img/ZA.svg";
+})
+
+relevancia.addEventListener('click', function(){
+    tipoFiltro.textContent = "relevancia";
+    setli();
+    relevancia.style.color = "#3483fa";
+    imgTipoFiltro.src = "img/relevancia.svg";
+})
+
+precioA.addEventListener('click', function(){
+    tipoFiltro.textContent = "precio";
+    setli();
+    precioA.style.color = "#3483fa";
+    imgTipoFiltro.src = "img/precioAscendente.svg";
+})
+
+precioD.addEventListener('click', function(){
+    tipoFiltro.textContent = "precio";
+    setli();
+    precioD.style.color = "#3483fa";
+    imgTipoFiltro.src = "img/precioDescendente.svg";
+})
