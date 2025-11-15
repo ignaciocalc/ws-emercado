f8e0180f00d6e37e5a7f2af766e0c07b73c0f003
Tacuabé Ferrari <inti.ferrari@fing.edu.uy>
2025-11-15 17:13:41 -0300

    realizados los estilos del index en todas las resoluciones. Agregadas imagenes para categorias

--- js/index.js
+++ js/index.js
@@ -1,17 +1,41 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        localStorage.setItem("redirect", true);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        localStorage.setItem("redirect", true);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        localStorage.setItem("redirect", true);
-        window.location = "products.html"
-    });
-});
+/*
+1) quitar estilos anteriores  [R]
+2) redefinir html para que se acople al figma (buscar tema carrousel) [R]
+ 2.1) primero defino el carrousel y después le doy estilo a las tarjetas [R]
+3) definir los estilos [R]
+4) arreglar mambos localstorage y seteo de imagenes
+*/
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.getElementById("autos").addEventListener("click", function() {
+//         localStorage.setItem("catID", 101);
+//         localStorage.setItem("redirect", true);
+//         window.location = "products.html"
+//     });
+//     document.getElementById("juguetes").addEventListener("click", function() {
+//         localStorage.setItem("catID", 102);
+//         localStorage.setItem("redirect", true);
+//         window.location = "products.html"
+//     });
+//     document.getElementById("muebles").addEventListener("click", function() {
+//         localStorage.setItem("catID", 103);
+//         localStorage.setItem("redirect", true);
+//         window.location = "products.html"
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
